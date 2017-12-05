f3082da7232b28bb8272bded3d0b004f719fb7f5
Saneyee Sarkar <Guest@epicodus-103.local>
2017-12-04 16:07:21 -0800

    angular js todo list

---
+++ app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+  <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+       <li>{{firstTask.description}}</li>
+     </ul>
+  </div>
+  `
+})
+
+
+export class AppComponent {
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstTask: Task = new Task("Finish weekend Angular homework for Epicodus course");
+}
+export class Task {
+public done: boolean = false;
+constructor(public description: string) { }
+}
