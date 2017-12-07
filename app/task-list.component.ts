c82919b9445cff4f6c3e3b714e0d5c981f4d49db
saneyee sarkar <saneyeesarkar@gmail.com>
2017-12-06 16:07:31 -0800

    adds child components and many things

---
+++ app/task-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from './task.model';
+
+@Component({
+  selector: 'task-list',
+  template: `
+  <ul>
+  <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}} <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
+  </ul>
+  `
+})
+
+export class TaskListComponent {
+   @Input() childTaskList: Task[];
+   @Output() clickSender = new EventEmitter();
+
+   editButtonHasBeenClicked(taskToEdit: Task) {
+    this.clickSender.emit(taskToEdit);
+  }
+
+
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("This task is done!");
+    } else {
+      alert("This task is not done. Better get to work!");
+    }
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+}
